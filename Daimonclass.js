ffb8db8841201b98eff062c760415a8cfc5d016d
Jeremy <bluej3710@gmail.com>
2019-10-22 15:15:24 -0400

    Day32buttonhtml/csswk7designlangcomponent

---
+++ Daimonclass.js
@@ -0,0 +1,13 @@
+//Writing a conditional statment
+var A = 0
+var close = 1
+var caristhere = true;
+var carisnotthere = false;
+function parkmycar(){
+    if(car = 0);
+    console.log("Park My Car");
+}else{
+    console.log("effthis");
+}
+parkmycar();
+
