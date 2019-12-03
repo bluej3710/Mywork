3a6c3c87f5a4e437dcb5c57cd2875d0932e432a2
Jeremy <bluej3710@gmail.com>
2019-12-03 15:00:43 -0500

    Week12TuesFastFoodDB

--- FastFoodApp.html/db.js
+++ FastFoodApp.html/db.js
@@ -1,3 +1,4 @@
+ /*FastFood Ex1 Mon*/
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -39,3 +40,55 @@ con.connect(function(err) {
 
   /*set to module.exports obj*/
   /*return 2functions to be json format*/
+
+
+
+
+   /*FastFood Ex2 Tues*/
+   var db = require(db.js)
+   // routes/index.js
+const noteRoutes = require('./note_routes');
+module.exports = function(app, db) {  noteRoutes(app, db); 
+  "you are requesting the index page"
+  
+  // note_routes.js
+module.exports = function(app, db) {  app.post('you are requesting the index page', (req, res) => {  // You'll create your note here. res.send('
+
+
+  //Server
+var createServer
+
+var http = require('http');
+
+// An example of a web server written with Node which responds with 'If you see this my server is working! lol ;)'.
+
+// To run the server, put the code into a file called example.js or NodejsServer4FastFoodApp.js and execute it with the node program.
+
+http.createServer(function (request, response) {
+
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+  response.end('If you see this my server is working! lol ;)\n');
+
+   // http header 
+   
+   var url = req.url; 
+     
+   if(url ==='/index') { 
+       res.write('you are requesting the index page');  
+       res.end();  
+   } 
+   else if(url ==='/contact') { 
+       res.write(' Welcome to contact us page');  
+       res.end();  
+   } 
+   else { 
+       res.write('Hello World!');  
+       res.end();  
+   } 
+
+}).listen(8080);
+
+
+//console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://swapi.co/documentation#root');
